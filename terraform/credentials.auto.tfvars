773cf080ac89c1077122c84926f9f4fb58613214
Techbonanza <dev@johnatangao.com>
2023-12-10 20:52:53 +0000

    terraform

---
+++ terraform/credentials.auto.tfvars
@@ -0,0 +1,22 @@
+proxmox_api_url = "value"
+proxmox_api_token_id = "value"
+proxmox_api_token_secret = "value"
+pm_node_name = "pve"
+pm_tls_insecure = true
+
+ssh_private_key = ""
+ssh_public_key = ""
+
+num_k3s_masters = 1
+num_k3s_nodes = 2
+num_k3s_masters_mem = 4096
+num_k3s_nodes_mem = 4096
+
+template_vm_name = "ubuntu-server-jammy"
+
+master_ips = [
+  "192.168.2.81"
+]
+worker_ips = [
+  "192.168.2.91"
+]
---
+++ terraform/variables.tf
@@ -0,0 +1,69 @@
+variable "proxmox_api_url" {
+    type = string
+}
+
+variable "proxmox_api_token_id" {
+    type = string
+    sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+    type = string
+    sensitive = true
+}
+
+variable "pm_tls_insecure" {
+  description = "Set to true to ignore certificate errors"
+  type        = bool
+  default     = false
+}
+
+variable "pm_host" {
+  description = "The hostname or IP of the proxmox server"
+  type        = string
+}
+
+variable "pm_node_name" {
+  description = "name of the proxmox node to create the VMs on"
+  type        = string
+  default     = "pve"
+}
+
+variable "ssh_private_key" {}
+variable "ssh_public_key" {}
+
+variable "num_k3s_masters" {
+  default = 1
+}
+
+variable "num_k3s_masters_mem" {
+  default = "4096"
+}
+
+variable "num_k3s_nodes" {
+  default = 2
+}
+
+variable "num_k3s_nodes_mem" {
+  default = "4096"
+}
+
+variable "template_vm_name" {
+  default = "ubuntu-server-jammy"
+}
+
+variable "master_ips" {
+  description = "List of ip addresses for master nodes"
+}
+
+variable "worker_ips" {
+  description = "List of ip addresses for worker nodes"
+}
+
+variable "networkrange" {
+  default = 24
+}
+
+variable "gateway" {
+  default = "192.168.3.1"
+}
