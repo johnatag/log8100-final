2c7f250f0245a1a6d5eab0137124d211429349d7
Techbonanza <dev@johnatangao.com>
2023-12-11 21:08:56 +0000

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,15 +11,15 @@ resource "proxmox_vm_qemu" "proxmox_vm_master" {
     clone = var.template_vm_name
 
     # VM System Settings
-    agent = 1
+    agent = "1"
 
     # VM CPU Settings
-    cores = 2
-    sockets = 1
+    cores = "2"
+    sockets = "1"
     cpu = "host"
 
     # VM Memory Settings
-    memory = var.num_k3s_masters_mem
+    memory = "${var.num_k3s_masters_mem}"
 
     network {
       bridge = "vmbr0"
@@ -37,7 +37,7 @@ resource "proxmox_vm_qemu" "proxmox_vm_master" {
 
     # (Optional) IP Address and Gateway
     ipconfig0 = "ip=${var.master_ips[count.index]}/${var.networkrange},gw=${var.gateway}"
-    nameserver = var.gateway
+    nameserver = "${var.gateway}"
     ciuser = "user"
 
     # (Optional) Add your SSH KEY
